a1154dd8b0b81af26f333305275e648506201da8
Laszlo Csontos <github@laszlocsontos.com>
2017-05-08 14:11:49 +0200

    SF

--- springuni-commons-jpa/src/main/java/com/springuni/commons/jpa/AbstractJpaRepositoryConfiguration.java
+++ springuni-commons-jpa/src/main/java/com/springuni/commons/jpa/AbstractJpaRepositoryConfiguration.java
@@ -51,7 +51,6 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
