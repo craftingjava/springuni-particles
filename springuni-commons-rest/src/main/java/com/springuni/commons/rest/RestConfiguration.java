cb0724f4f5468c7a014d36230b2d1f5c9d6b5aed
Laszlo Csontos <github@laszlocsontos.com>
2017-05-10 12:24:23 +0200

    Register controller advice

--- springuni-commons-rest/src/main/java/com/springuni/commons/rest/RestConfiguration.java
+++ springuni-commons-rest/src/main/java/com/springuni/commons/rest/RestConfiguration.java
@@ -2,6 +2,7 @@ package com.springuni.commons.rest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.ControllerAdviceBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
@@ -14,8 +15,18 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 public class RestConfiguration extends WebMvcConfigurationSupport {
 
   @Bean
+  public ControllerAdviceBean controllerAdviceBean() {
+    Object controllerAdvice = createControllerAdvice();
+    return new ControllerAdviceBean(controllerAdvice);
+  }
+
+  @Bean
   public DefaultController defaultController() {
     return new DefaultController();
+  }
+
+  protected Object createControllerAdvice() {
+    return new RestErrorHandler();
   }
 
   @Override
