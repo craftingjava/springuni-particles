f2b99679fdb39193487b9b9eeb8c61c067dff623
Laszlo Csontos <github@laszlocsontos.com>
2017-05-12 06:41:38 +0200

    Add custom ObjectMapper

--- springuni-commons-rest/src/main/java/com/springuni/commons/rest/RestConfiguration.java
+++ springuni-commons-rest/src/main/java/com/springuni/commons/rest/RestConfiguration.java
@@ -1,9 +1,17 @@
 package com.springuni.commons.rest;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_ABSENT;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
@@ -28,11 +36,30 @@ public class RestConfiguration extends WebMvcConfigurationSupport {
     return modelMapper;
   }
 
+  @Bean
+  public ObjectMapper objectMapper() {
+    // Registers com.fasterxml.jackson.datatype.jsr310.JavaTimeModule automatically
+    ObjectMapper objectMapper = Jackson2ObjectMapperBuilder
+        .json()
+        .indentOutput(true)
+        .featuresToDisable(WRITE_DATES_AS_TIMESTAMPS)
+        .serializationInclusion(NON_ABSENT)
+        .build();
+    customizeObjectMapper(objectMapper);
+    return objectMapper;
+  }
+
   @Override
   @Bean
   @DependsOn("controllerAdvice")
   public RequestMappingHandlerAdapter requestMappingHandlerAdapter() {
     return super.requestMappingHandlerAdapter();
+  }
+
+  @Override
+  protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    ObjectMapper objectMapper = objectMapper();
+    converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
   }
 
   protected Object createControllerAdvice() {
@@ -44,6 +71,9 @@ public class RestConfiguration extends WebMvcConfigurationSupport {
   }
 
   protected void customizeModelMapper(ModelMapper modelMapper) {
+  }
+
+  protected void customizeObjectMapper(ObjectMapper objectMapper) {
   }
 
   @Override
